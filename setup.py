7a9783091be750bb90f85caaa5a40a0c078a3531
bnorthan <bnorthan@gmail.com>
2021-04-29 09:44:30 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='tnia-python',
+      version='0.1',
+      description='A collection of utilities',
+      url='http://github.com/tnia/tnia-python',
+      author='Brian Northan',
+      author_email='bnorthan@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
