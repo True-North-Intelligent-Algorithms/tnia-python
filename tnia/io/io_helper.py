9e42261058834a3a4bd7b609b4895e48d9c2ff19
bnorthan <bnorthan@gmail.com>
2021-11-02 07:51:31 -0400

    Add stack opener

--- tnia/io/io_helper.py
+++ tnia/io/io_helper.py
@@ -1,5 +1,6 @@
 import os
 from glob import glob
+import numpy as np
 
 def get_file_names_from_dir(dir, extension):
     """ gets all file names with extension from directory
@@ -9,4 +10,15 @@ def get_file_names_from_dir(dir, extension):
         extension (string): extension with no '.' ie 'tif', 'JPG'
     """
     return [y for x in os.walk(dir) for y in glob(os.path.join(x[0], '*.'+extension))]
+
+def open_stack(dir, extension, opener):
+    file_names = get_file_names_from_dir(dir, extension)
+
+    stacked = []
+    for f in file_names:
+        im=opener(f)
+        print(f)
+        print(im.shape)
+        stacked.append(im)
     
+    return np.stack(stacked)
