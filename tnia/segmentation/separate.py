0f523d0c415d30f326a815b59708d6fdebff2e3a
bnorthan <bnorthan@gmail.com>
2021-11-15 12:41:14 -0500

    Add a second version of separate
    This is called 'separate_touching2'.  It uses a hybrid of the scikit
    strategy and the clij strategy for pre-processing to watershed.  Like
    the clij strategy it applies a blur to the original image to detect
    local max to be used for seeds.  Then a second (usually smaller) blur
    is applied top the original image and used to create a distance map,
    seed points, distance map, and Otsu of the original are then fed to the
    scikitimage watershed function.

--- tnia/segmentation/separate.py
+++ tnia/segmentation/separate.py
@@ -7,7 +7,10 @@ from skimage.feature import peak_local_max
 from skimage.measure import label
 from skimage.morphology import closing, disk, erosion
 from skimage.segmentation import find_boundaries, watershed
-
+from skimage.filters import gaussian, threshold_otsu
+from tnia.viewing.napari_helper import show_image_and_label as sil
+from skimage.morphology import remove_small_holes
+from tnia.morphology.fill_holes import fill_holes_3d_slicer
 def separate_touching(segmented, min_distance, num_erosions):
     """ separates touching objects using a watershed process
 
@@ -24,6 +27,7 @@ def separate_touching(segmented, min_distance, num_erosions):
     #eroded = erosion(closed, disk(2))
     distance = ndi.distance_transform_edt(segmented)
     coords = peak_local_max(distance, min_distance=min_distance, exclude_border=False)
+    print(coords.shape)
     mask = np.zeros(distance.shape, dtype=bool)
     mask[tuple(coords.T)] = True
     markers, _ = ndi.label(mask)
@@ -34,6 +38,42 @@ def separate_touching(segmented, min_distance, num_erosions):
     for i in range(num_erosions):
         separated = erosion(separated, disk(1))
 
-    return separated 
+    return separated, labels, distance
 
+def separate_touching2(im, min_distance, spot_sigma, distance_sigma):
+    """ This algorithm uses a strategy similar to the one described here in
+    https://clij.github.io/clij2-docs/md/voronoi_otsu_labeling/
 
+    The main difference is instead of calling Voronoi labeling we create
+    a distance map and use it as the input to skimage.segmentation.watershed
+
+    Args:
+        im (numpy array): input image 
+        min_distance (number): min distance between separated objects 
+        spot_sigma (array): sigma in each dimension for blur applied before spot detection
+        distance_sigma ([type]): sigma in each dimension for blur applied before distance map
+    """
+    binary = im>threshold_otsu(im)
+
+    fill_holes_3d_slicer(binary)
+
+    blurred_spot = gaussian(im, spot_sigma)
+
+    # calculate peaks from blurred image
+    coords = peak_local_max(blurred_spot, min_distance)
+    print(coords.shape)
+    mask = np.zeros(im.shape, dtype=bool)
+    mask[tuple(coords.T)] = True
+    markers, _ = ndi.label(mask)
+
+    # calculate distance map from second blurred image
+    blurred_distance = gaussian(im, distance_sigma)
+    binary_distance = blurred_distance>threshold_otsu(blurred_distance)
+ 
+    fill_holes_3d_slicer(binary_distance)
+    
+    distance = ndi.distance_transform_edt(binary_distance)
+
+    labels = watershed(-distance, markers, mask=binary,watershed_line=True)
+
+    return labels,distance,binary
