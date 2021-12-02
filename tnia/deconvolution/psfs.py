1754680259447f3b0f8e49165cbb5acb282db135
bnorthan <bnorthan@gmail.com>
2021-12-02 18:25:27 -0500

    Add psf_from_beads function

--- tnia/deconvolution/psfs.py
+++ tnia/deconvolution/psfs.py
@@ -1,6 +1,8 @@
 import microscPSF.microscPSF as msPSF
 import numpy as np
+from skimage.filters import threshold_otsu
 from numpy.fft import ifftn, ifftshift, fftshift
+from tnia.segmentation.rendering import draw_centroids 
 
 def gibson_lanni_3D(NA, ni, ns, voxel_size_xy, voxel_size_z, xy_size, z_size, pz, wvl):
     m_params = msPSF.m_params
@@ -47,3 +49,32 @@ def paraxial_psf(n, wavelength, numerical_aperture, pixel_size):
     otf = paraxial_otf(n, wavelength, numerical_aperture, pixel_size)
     psf = fftshift(ifftn(ifftshift(otf)).astype(np.float32))
     return psf/psf.sum()
+
+def psf_from_beads(bead_image, background_factor=1.25):
+    """ Extracts a PSF from a bead image using reverse deconvolution (a.k.a. PSF Distilling)
+
+    Args:
+        bead_image (numpy array): an image of a field of sub-resolution beads
+        background_factor (float, optional): Used to modulate background subtraction. Defaults to 1.25.
+
+    Returns:
+        [numpy array]: the PSF
+    """
+    bead_image=bead_image-background_factor*bead_image.mean()
+    bead_image[bead_image<=0]=.1
+
+    thresholded = bead_image>threshold_otsu(bead_image)
+
+    centroids = draw_centroids(thresholded)
+
+    from clij2fft.richardson_lucy import richardson_lucy
+    #from skimage.restoration import richardson_lucy
+    im_32=bead_image.astype('float32')
+    centroids_32=centroids.astype('float32')
+    print('call skimage rl')
+    psf=richardson_lucy(im_32, centroids_32, 200)
+
+    return psf
+
+
+
