f7b8e17e5cbb618ab3ceb0f81859c2f9d4d1ab75
bnorthan <bnorthan@gmail.com>
2021-12-03 18:18:15 -0500

    Add the test for the ABRF calibratrion image

---
+++ tests/segmentation/abrf-lmrg-calibration.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from skimage.io import imread
+from skimage.filters import threshold_otsu
+from skimage.measure import label, regionprops
+import os
+
+image_name = 'D:\\images\\ABRF LMRG Image Analysis Study\\calibration\\calibration.tiff'
+out_dir = 'D:\\images\\ABRF LMRG Image Analysis Study\\nuclei\\out\\'
+
+im = imread(image_name)
+thresh = im>threshold_otsu(im)
+labeled = label(thresh)
+object_list = regionprops(labeled,im)
+
+stats=pd.DataFrame(columns=['x','y','z','intensity','volume'])
+
+for o in object_list:
+    c=o.centroid
+    v=o.area
+    i=v*o.mean_intensity
+
+    stats.loc[len(stats)]=[c[2],c[1],c[0],i,v]
+
+csv_name = out_dir+'northan_brian_calibration.csv'
+stats.to_csv(csv_name)
