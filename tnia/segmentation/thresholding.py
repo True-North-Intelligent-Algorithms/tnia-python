68c06a00c9e50842e231e69c6945ade1d43cfc7c
bnorthan <bnorthan@gmail.com>
2022-02-04 16:18:03 -0500

    Add local global threshold 3d that operates slicewise

--- tnia/segmentation/thresholding.py
+++ tnia/segmentation/thresholding.py
@@ -13,3 +13,11 @@ def local_global_threshold(image, local_window_size, global_threshold):
         [np array]: And of local and global thresholded image
     """
     return np.logical_and(image>threshold_local(image, local_window_size), image>global_threshold)
+
+def local_global_threshold_3d(image, local_window_size, global_threshold):
+    out = np.zeros_like(image)
+    for i in range(image.shape[0]):
+        out[i,:,:]=local_global_threshold(image[i,:,:], local_window_size, global_threshold)
+    
+    return out
+
