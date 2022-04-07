ecc0d9e28c9f6f2b1afc394aca8e671d0363c594
bnorthan <bnorthan@gmail.com>
2022-04-06 20:32:09 -0400

    Add functions for single figure and title

--- tnia/reports/markdown.py
+++ tnia/reports/markdown.py
@@ -1,12 +1,33 @@
 
 
+
+def title(title, level=1):
+
+    if level==2:
+        markdown='## '
+    else:
+        markdown='# '
+
+    markdown=markdown+title+' \n\n'
+    
+    return markdown
+
+def figure(fig, title, fig_title, report_dir):
+    fig_name = fig_title+'.png' 
+    fig.savefig(report_dir+fig_name)
+
+    markdown = '## '+title+' \n\n'
+    markdown+='![]('+fig_name+')  \n\n'
+
+    return markdown
+
 def image_test(title, report_dir, info_list, figs):
-    """ generates a snippet of markdown with a title, a list of information, and a plot
+    """ generates a snippet of markdown with a title, a list of information, and list of plots
 
     Args:
         title (string): Title of the test
         info_list (list of strings): A list of info to print in the markdown
-        fig (matplotlib figure): figure to print at end of test
+        figs (list of matplotlib figures): figures to print at end of test
     """
     
     markdown='## '+title+' \n\n'
