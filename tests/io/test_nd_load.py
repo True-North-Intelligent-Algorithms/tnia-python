520f8f86713517dfe1d08d13601bd0bd61807eba
bnorthan <bnorthan@gmail.com>
2022-04-06 20:37:13 -0400

    Tweak test parameters

--- tests/io/test_nd_load.py
+++ tests/io/test_nd_load.py
@@ -3,7 +3,9 @@ import bioformats
 
 bfh.start_jvm()
 
-filename='D:\\elephasbio\\large montage\\720x AtlasVolume-05042021-0833-006\\720x AtlasVolume-05042021-0833-006.xml'
+#filename='D:\\elephasbio\\large montage\\720x AtlasVolume-05042021-0833-006\\720x AtlasVolume-05042021-0833-006.xml'
+filename='D:\\images\\From Joao Mamede\\F2_laminigfpcarub8am.nd2'
+
 
 meta=bioformats.get_omexml_metadata(filename)
 o=bioformats.OMEXML(meta)
--- tests/io/test_nd_save.py
+++ tests/io/test_nd_save.py
@@ -1,20 +1,28 @@
 import numpy as np
 import bioformats as bf
+import tifffile
 import tnia.io.bioformats_helper as bfh
 import tnia.io.tifffile_helper as tfh
 # 4 channels
-nc=4
-nz=5
-# 512 by 512
-ny = 512
-nx = 512
 
-test=((2**16-1)*np.random.rand(nc, nz, ny, nx)).astype('uint16')
+nz=500
+nc=4
+# 512 by 512
+ny = 2048
+nx = 1024
+
+test=((2**16-1)*np.random.rand(nz, nc, ny, nx)).astype('float32')
+
+tifffile.imwrite('from_tifffile.tif', test, imagej=True, metadata={'axes': 'ZCYX', 'mode':'composite'})
+tfh.save_zcyx('from_helper.tif', test)
+
 '''
 import tifffile
 test = np.moveaxis(test,0,1)
 tfh.save_zcyx('fromthetiffhelper.tif', test)
 #bfh.save_4D('fromthehelper2.tif',test)
+'''
+
 '''
 import imagej
 import xarray as xr
@@ -25,3 +33,4 @@ data = xr.DataArray(test, dims=('C','z','y','x'))
 dataset = dataset = ij.py.to_dataset(data)
 dataset = ij.py.to_dataset(data)
 ij.io().save(dataset, 'frompyfiji.tif')
+'''
