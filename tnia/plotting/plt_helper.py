a8317fc7d060cf7d75eed60e3fb4c6ab1d93c6e8
bnorthan <bnorthan@gmail.com>
2022-05-03 14:20:23 -0400

    Flatten axes so zip works properly

--- tnia/plotting/plt_helper.py
+++ tnia/plotting/plt_helper.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def imshow2d(im, width=8, height=6):
     """ a little helper to show images of differnt sizes using pyplot.  Just makes calls to imshow more readable. 
@@ -16,10 +17,13 @@ def imshow2d(im, width=8, height=6):
     return fig
 
 def imshow_multi2d(ims, titles, rows, cols, width=10, height=4):
-    """ a little helper to show images of differnt sizes using pyplot.  Just makes calls to imshow more readable. 
+    """ a little helper to show a grid of images of differnt sizes using pyplot.  Just makes calls to imshow more readable. 
 
     Args:
-        im ([type]): input image
+        ims (list of np arrays): input images
+        titles(list of strings): titles of images
+        rows: number of rows of images
+        cols: number of collumns of images (not rows*cols needs to equal number of images) 
         width (int, optional): Width of plot. Defaults to 8.
         height (int, optional): Height of plot. Defaults to 6.
 
@@ -27,11 +31,14 @@ def imshow_multi2d(ims, titles, rows, cols, width=10, height=4):
         [type]: returns the figure
     """
     fig, axes = plt.subplots(rows, cols, figsize=(width,height))
-    
-    for im,ax,title in zip(ims,axes,titles):
+
+    for im,ax,title in zip(ims,np.ndarray.flatten(axes),titles):
+        #print('types')
+        #print(type(im), type(ax), type(title))
+        #print()
         ax.imshow(im)
         ax.set_title(title)
 
-    return fig
+    return fig 
 
 #def plot_line(line, tittle)
