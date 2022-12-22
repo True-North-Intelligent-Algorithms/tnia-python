19523a3a3b3af7f316bb1b282e8ce94b273cb89b
bnorthan <bnorthan@gmail.com>
2022-12-21 20:32:57 -0500

    Use gray colormap

--- tnia/deeplearning/dl_helper.py
+++ tnia/deeplearning/dl_helper.py
@@ -90,7 +90,7 @@ def plot_img_label(img, lbl, img_title="image", lbl_title="label", **kwargs):
     """
     
     fig, (ai,al) = plt.subplots(1,2, figsize=(12,5), gridspec_kw=dict(width_ratios=(1.25,1)))
-    im = ai.imshow(img, cmap='gray', clim=(0,1))
+    im = ai.imshow(img, cmap='gray')
     ai.set_title(img_title)    
     fig.colorbar(im, ax=ai)
     al.imshow(lbl, random_label_cmap())
