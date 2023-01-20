0676f753b9a961a685661af36224d01fa3a21c61
bnorthan <bnorthan@gmail.com>
2023-01-20 15:35:57 -0500

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tnia-python',
-      version='0.1.6',
+      version='0.1.7',
       description='A collection of utilities',
       url='http://github.com/tnia/tnia-python',
       author='Brian Northan',
