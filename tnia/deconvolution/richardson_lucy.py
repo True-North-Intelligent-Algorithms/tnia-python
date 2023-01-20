88e962ad36e350f34301874b7cfb014e068cafa2
bnorthan <bnorthan@gmail.com>
2023-01-20 15:34:47 -0500

    Iterate on cuda and numpy Richardson Lucy iterations

--- tnia/deconvolution/richardson_lucy.py
+++ tnia/deconvolution/richardson_lucy.py
@@ -1,40 +1,47 @@
 import numpy as np
 from numpy.fft import fftn, ifftn, fftshift 
 import cupy as cp
+from tnia.deconvolution.pad import pad
 
 def richardson_lucy_np(image, psf, num_iters):
-    
+    if (image.shape != psf.shape):
+        print('padding psf')
+        psf,_=pad(psf, image.shape, 'constant')
+   
     otf = fftn(fftshift(psf))
     otf_ = np.conjugate(otf)    
     estimate = image#np.ones(image.shape)/image.sum()
 
+    print()
     for i in range(num_iters):
-        print(i)
-        
-        reblurred = ifftn(fftn(estimate) * otf)
+        print(i, end =" ")
+        reblurred = np.real(ifftn(fftn(estimate) * otf))
         ratio = image / (reblurred + 1e-30)
-        estimate = estimate * (ifftn(fftn(ratio) * otf_)).astype(float)
-        
-
+        correction =  np.real((ifftn(fftn(ratio) * otf_)).astype(float))
+        estimate = estimate * correction
+    print()
     return estimate
 
 def richardson_lucy_cp(image, psf, num_iters):
-    print('1')
+    if (image.shape != psf.shape):
+        print('padding psf')
+        psf,_=pad(psf, image.shape, 'constant')
+    
     image = cp.array(image)
-    print('2')
     psf = cp.array(psf)
     
-    print('3')
     otf = cp.fft.fftn(cp.fft.fftshift(psf))
-    print('4')
+    otf_ = cp.conjugate(otf)
     estimate = image#np.ones(image.shape)/image.sum()
 
-    print('iiiiiii')
+    print()
     for i in range(num_iters):
-        print('cupy',i)
-       
-        reblurred = cp.fft.ifftn(cp.fft.fftn(estimate) * otf)
+        print(i, end =" ")
+        reblurred = np.real(cp.fft.ifftn(cp.fft.fftn(estimate) * otf))
+
         ratio = image / (reblurred + 1e-30)
-        estimate = estimate * (cp.fft.ifftn(cp.fft.fftn(ratio) * otf)).astype(float)
-        
+        correction=cp.real((cp.fft.ifftn(cp.fft.fftn(ratio) * otf_)))
+        estimate = estimate * correction 
+    print()        
     return cp.asnumpy(estimate)
+    
