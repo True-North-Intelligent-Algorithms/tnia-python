2f1daca99141a4874a8322693c03ae68b07c5451
bnorthan <bnorthan@gmail.com>
2023-02-20 21:27:02 -0500

    Use cupy for Poisson noise
    Adding Poisson takes a long time for high signal, switching to cupy for
    this step.  TODO:  Make cupy optional so that it still runs on non-cuda
    systems.

--- tnia/deconvolution/forward.py
+++ tnia/deconvolution/forward.py
@@ -1,16 +1,55 @@
-from numpy.fft import fftn, ifftn, fftshift 
-from numpy.random import poisson
+from numpy.fft import fftn, ifftn, ifftshift 
+from cupy.random import poisson
+import cupy as cp
+from tnia.deconvolution.pad import pad, unpad
 
-def forward(field, psf, max_photons, background_level, add_poisson=True):
+
+def forward(field, psf, background_level, add_poisson=True):
     '''
-        Note this function inspired by code from https://github.com/jdmanton/rl_positivity_sim by James Manton
+    Perform forward imaging on an input field using a point spread function (PSF).
+
+    Parameters:
+    -----------
+    field : ndarray
+        The input field to be imaged. 
+    psf : ndarray
+        The point spread function (PSF) to be used for imaging. 
+    background_level : float
+        The background level to be added to the image.
+    add_poisson : bool, optional
+        Whether or not to add Poisson noise to the image. Default is True.
+
+    Returns:
+    --------
+    ndarray
+        The forward-imaged field, with the same shape as the input field.
+
+    Notes:
+    ------
+    This function is inspired by code from https://github.com/jdmanton/rl_positivity_sim by James Manton.
     '''
-    otf = fftn(fftshift(psf))
+
+    # compute the extended size of the image and psf
+    extended_size = [field.shape[i]+2*int(psf.shape[i]/2) for i in range(len(field.shape))]
+    original_size = field.shape
+    
+    # pad the image and psf to the extended size computed above
+    field,_=pad(field, extended_size, 'constant')
+    psf,_=pad(psf, extended_size, 'constant')
+    
+    # perform forward imaging 
+    otf = fftn(ifftshift(psf))
     field_imaged = ifftn(fftn(field)*otf)
-    field_imaged = field_imaged/field_imaged.max()
-    field_imaged = field_imaged*max_photons+background_level
+    field_imaged = field_imaged+background_level
 
+    field_imaged = cp.array(field_imaged)
+    
+    # unpad before poisson noise is added
+    field_imaged=unpad(cp.asnumpy(field_imaged), original_size)
+
+    # add poisson noise   
     if add_poisson:
-        return poisson(field_imaged.astype(float))
-    else:
-        return field_imaged.astype(float)
+        field = poisson(field_imaged.astype(float))
+
+    # return field as numpy array
+    return cp.asnumpy(field).astype('float32')
