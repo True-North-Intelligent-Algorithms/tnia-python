184216f39fa1ac1556c35a2e6bddf9001032248c
Brian Northan <bnorthan@gmail.com>
2023-04-18 14:44:52 -0400

    Add utility to center crop by size units

--- tnia/nd/ndutil.py
+++ tnia/nd/ndutil.py
@@ -20,6 +20,39 @@ def centercrop(im, shape):
     slices = [slice(start_indices[i], start_indices[i] + shape[i]) for i in range(len(im.shape))]
 
     return im[tuple(slices)]
+    
+def centercrop_bysize(img, size, spacing):
+    """
+    Center crops the input image to the desired size with respect to the spacing.
+
+    This function mostly written by Tim-Oliver Buchholz, tibuch on github, as part of the https://github.com/fmi-faim/napari-psf-analysis plugin for napari.  
+
+    napari-psf-analysis is licensed under the BDS 3-Clause License,
+    
+    tibuch
+
+    
+    Parameters:
+    ----------
+        img (numpy array): the image to crop
+        size (tuple): the size of the crop in the same units as spacing
+        spacing (tuple): the spacing of the image
+
+    Returns:
+    -------
+        crop (numpy array): the cropped and centered PSF
+    """
+    z, y, x = np.unravel_index(np.argmax(img), img.shape)
+    
+    half_size = np.ceil(size / spacing / 2).astype(int)
+    
+    z_slice = slice(max(0, z - half_size[0]), min(z + half_size[0] + 1, img.shape[0]))
+    y_slice = slice(max(0, y - half_size[1]), min(y + half_size[1] + 1, img.shape[1]))
+    x_slice = slice(max(0, x - half_size[2]), min(x + half_size[2] + 1, img.shape[2]))
+    
+    
+    crop = img[z_slice, y_slice, x_slice]
+    return crop
 
 
 def makergb(r,g,b):
