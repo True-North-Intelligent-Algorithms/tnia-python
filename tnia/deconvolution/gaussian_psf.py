985402ec9ed4e8c8af2a82afb697e01de63fdc52
bnorthan <bnorthan@gmail.com>
2023-06-16 11:08:09 -0400

    Fix hardcoded grid coordinates

--- tnia/deconvolution/gaussian_psf.py
+++ tnia/deconvolution/gaussian_psf.py
@@ -15,7 +15,7 @@ def gaussian_3d(xy_dim, z_dim, xy_sigma, z_sigma):
     """
     muu = 0.0
     gauss = np.empty([z_dim,xy_dim,xy_dim])
-    x_, y_, z_ = np.meshgrid(np.linspace(-10,10,xy_dim), np.linspace(-10,10,xy_dim), np.linspace(-10,10,z_dim))
+    x_, y_, z_ = np.meshgrid(np.linspace(-(xy_dim//2),xy_dim//2,xy_dim), np.linspace(-(xy_dim//2),xy_dim//2,xy_dim), np.linspace(-(z_dim//2),z_dim//2,z_dim))
     for x in range(xy_dim):
         for y in range(xy_dim):
             for z in range(z_dim):
@@ -36,14 +36,14 @@ def gaussian_2d(xy_dim, xy_sigma):
     Parameters:
     ----------
         xy_dim (int): the size of the PSF in the xy plane
-        xy_sigma (float): the sigma of the Gaussian in the xy plane
+        xy_sigma (float): the sigma of the Gaussian in the xy plane in pixels
     Returns:
     -------
         psf (numpy array): the PSF
     """
     muu = 0.0
     gauss = np.empty([xy_dim,xy_dim])
-    x_, y_ = np.meshgrid(np.linspace(-10,10,xy_dim), np.linspace(-10,10,xy_dim))
+    x_, y_ = np.meshgrid(np.linspace(-(xy_dim//2),xy_dim//2,xy_dim), np.linspace(-(xy_dim//2),xy_dim//2,xy_dim))
     for x in range(xy_dim):
         for y in range(xy_dim):
             tx=x_[x,y]
@@ -51,7 +51,7 @@ def gaussian_2d(xy_dim, xy_sigma):
             
             gauss[y,x]=np.exp(-( (tx-muu)**2 / ( 2.0 * xy_sigma**2 ) ) )*np.exp(-( (ty-muu)**2 / ( 2.0 * xy_sigma**2 ) ) )
 
-    gauss=gauss+0.000000000001
+    #gauss=gauss+0.000000000001
     gauss=gauss/gauss.sum()
 
     return gauss
