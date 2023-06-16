318c05d6f64050fddc88d605104495356fd4a969
bnorthan <bnorthan@gmail.com>
2023-06-16 10:32:37 -0400

    Clean up np/cp use, make cp version memory efficient
    Make the cupy version memory efficient by explicitly deleting arrays
    right after we are finished with them, also make sure we delete the fft
    plan cache (todo, consider making this optional for scenario we are
    re-using same FFT size).
    
    Move the cupy imports to the cupy block, so numpy version will still
    work even if cupy isn't installed.
    
    Todo:  Long term consider refactoring cupy, numpy into 2 files or
    smarter imports, so we aren't constantly importing cupy (the overhead
    is fairly small when processing large 3D data, but could be significant
    with 2d data)

--- tnia/deconvolution/forward.py
+++ tnia/deconvolution/forward.py
@@ -1,16 +1,8 @@
 from numpy.fft import fftn, ifftn, ifftshift 
-from cupy.random import poisson
-import cupy as cp
+import numpy as np
 from tnia.deconvolution.pad import pad, unpad, get_next_smooth
-from tnia.utility.cupy_checker import get_platform
 
-
-def add_poisson_noise(img):
-    img = cp.array(img)
-    img = poisson(img.astype(float))
-    return cp.asnumpy(img).astype('float32')
-
-def forward(field, psf, background_level, add_poisson=True):
+def forward(field, psf, background_level, add_poisson=True, gpu=False):
     '''
     Perform forward imaging on an input field using a point spread function (PSF).
 
@@ -24,6 +16,8 @@ def forward(field, psf, background_level, add_poisson=True):
         The background level to be added to the image.
     add_poisson : bool, optional
         Whether or not to add Poisson noise to the image. Default is True.
+    gpu : bool, optional
+        Whether or not to use the GPU for computation. Default is False.
 
     Returns:
     --------
@@ -32,7 +26,7 @@ def forward(field, psf, background_level, add_poisson=True):
 
     Notes:
     ------
-    This function is inspired by code from https://github.com/jdmanton/rl_positivity_sim by James Manton.
+    This function is partly inspired by code from https://github.com/jdmanton/rl_positivity_sim by James Manton.
     '''
 
     # compute the extended size of the image and psf
@@ -44,25 +38,70 @@ def forward(field, psf, background_level, add_poisson=True):
     field,_=pad(field, extended_size, 'constant')
     psf,_=pad(psf, extended_size, 'constant')
     
-    # perform forward imaging 
-    otf = fftn(ifftshift(psf))
-    field_imaged = ifftn(fftn(field)*otf)
-    field_imaged = field_imaged+background_level
-    
-    # unpad before poisson noise is added
-    field_imaged=unpad(field_imaged, original_size)
+    if gpu==False:
+        # perform forward imaging 
+        otf = fftn(ifftshift(psf))
+        field_imaged = ifftn(fftn(field)*otf)
+        field_imaged = field_imaged+background_level
+        
+        # unpad before poisson noise is added
+        field_imaged=unpad(field_imaged, original_size)
 
-    field_imaged = cp.array(field_imaged)
-    
-    # add poisson noise   
-    if add_poisson:
-        field_imaged = poisson(field_imaged.astype(float))
+        # add poisson noise   
+        if add_poisson:
+            field_imaged = np.random.poisson(field_imaged.astype(float))
 
-    # return field as numpy array
-    return cp.asnumpy(field_imaged).astype('float32')
+        # return field as numpy array
+        return field_imaged    
+    else:
+        # perform forward imaging on GPU, using cupy, del and mempool.free_all_blocks() is used to delete variables and free memory
+        import cupy as cp
+        
+        temp = ifftshift(psf)
+        
+        mempool=cp.get_default_memory_pool()
+        psf_shift_cp = cp.array(temp)
+        otf_cp = cp.fft.rfftn(psf_shift_cp)
+        del psf_shift_cp
+        mempool.free_all_blocks()
+        
+        field_cp = cp.array(field)
+        field_fft_cp = cp.fft.rfftn(field_cp)
+        del field_cp
 
+        forward_fft_cp = field_fft_cp*otf_cp
+        del field_fft_cp
+        del otf_cp
 
-def convolve(x, h):
-    pl = get_platform(x)
-    return pl.fft.irfftn(pl.fft.rfftn(x) * pl.fft.rfftn(pl.fft.ifftshift(h)), x.shape)
+        field_imaged_cp = cp.fft.irfftn(forward_fft_cp, field.shape)
+        del forward_fft_cp
+        
+        field_imaged_cp_plus_background = field_imaged_cp+background_level
+        del field_imaged_cp
+        
+        field_imaged = cp.asnumpy(field_imaged_cp_plus_background)
+        del field_imaged_cp_plus_background
+        
+        # unpad before poisson noise is added
+        field_imaged=unpad(field_imaged, original_size)
+        
+        # add poisson noise   
+        if add_poisson:
+            field_imaged_cp = cp.array(field_imaged)
+            field_imaged_poisson_cp = cp.random.poisson(field_imaged_cp)
+            del field_imaged_cp
+            field_imaged = cp.asnumpy(field_imaged_poisson_cp)
+            del field_imaged_poisson_cp
+        else:
+            field_imaged = cp.asnumpy(field_imaged)
+
+        # explicitly clear the plan cache to avoid memory leak
+        cache = cp.fft.config.get_plan_cache()
+        cache.clear()
+        
+        cp.get_default_memory_pool().free_all_blocks()
+        # return field as numpy array
+        
+        return field_imaged.astype('float32')
+
 
