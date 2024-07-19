331e365be71223e0be87ad90856bbc20e9a3faf2
bnorthan <bnorthan@gmail.com>
2024-07-19 15:28:31 -0400

    Revert change that broke some examples

--- tnia/deeplearning/dl_helper.py
+++ tnia/deeplearning/dl_helper.py
@@ -381,13 +381,13 @@ def make_random_patch(img, truth, patch_size, axes, ind=None, sub_sample_xy=1):
 
     return img_crop, truth_crop, ind, truth_ind
 
-def collect_training_data(data_path, sub_sample_image_collection=1, downsample=False,pmin=3, pmax=99.8, normalize_input=True, normalize_truth=False, training_multiple=1, patch_size=None, add_trivial_channel=True):
+def collect_training_data(data_path, sub_sample=1, downsample=False,pmin=3, pmax=99.8, normalize_input=True, normalize_truth=False, training_multiple=1, patch_size=None, add_trivial_channel=True):
     """
     Collect training data for image processing models.
 
     Parameters:
     data_path (str): Path to the directory containing input and ground truth data.
-    sub_sample_image_collection (int, optional): Wether to subsample the images in the collection. Defaults to 1.  
+    sub_sample (int, optional): Wether to subsample the images in the collection. Defaults to 1.  
         for example if sumbsample is 2, every other image will be used. 
     downsample (bool, optional): If True, downsample the images by a factor of 2. Defaults to False.
     pmin (float, optional): Minimum percentile for normalization. Defaults to 3.
@@ -418,8 +418,8 @@ def collect_training_data(data_path, sub_sample_image_collection=1, downsample=F
     input_path = os.path.join(data_path, "input" + str(i))
     truth_path = os.path.join(data_path, "ground truth" + str(i))
 
-    input_files = os.listdir(input_path)[0::sub_sample_image_collection]
-    truth_files = os.listdir(truth_path)[0::sub_sample_image_collection]  
+    input_files = os.listdir(input_path)[0::sub_sample]
+    truth_files = os.listdir(truth_path)[0::sub_sample]  
 
     for i in range(len(input_files)):
         # Load the corrupted image and ground truth image
@@ -439,10 +439,10 @@ def collect_training_data(data_path, sub_sample_image_collection=1, downsample=F
 
         if normalize_input:
             # Normalize the pixel values to [0, 1]
-            input_img =  normalize(input_img, pmin, pmax)
+            input_img =  quantile_normalization(input_img, pmin/100., pmax/100.)
 
         if (normalize_truth):
-            ground_truth_img = normalize(ground_truth_img, pmin, pmax)
+            ground_truth_img = quantile_normalization(ground_truth_img, pmin/100., pmax/100.)
 
         if (training_multiple>1):
             new_z = training_multiple*(input_img.shape[0]//training_multiple)
