90ff2582e8547ab5b2eba5206b1c5b3159527099
bnorthan <bnorthan@gmail.com>
2024-10-26 09:35:08 -0400

    Vendor Cellpose mask_overlay function

--- tnia/plotting/plt_helper.py
+++ tnia/plotting/plt_helper.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import PowerNorm
+import colorsys
 
 def random_label_cmap(n=2**16, h = (0,1), l = (.4,1), s =(.2,.8)):
     import matplotlib
@@ -146,3 +147,48 @@ def draw_bimodal_hist(data, thresh, params, ax=None, title='bimodal hist'):
     else:
         fig=None
 '''
+
+
+def hsv_to_rgb(arr):
+    hsv_to_rgb_channels = np.vectorize(colorsys.hsv_to_rgb)
+    h, s, v = np.rollaxis(arr, axis=-1)
+    r, g, b = hsv_to_rgb_channels(h, s, v)
+    rgb = np.stack((r, g, b), axis=-1)
+    return rgb
+
+
+def mask_overlay(img, masks, colors=None):
+    """Overlay masks on image (set image to grayscale).
+
+    (Vendored from Cellpose codebase)
+
+    Args:
+        img (int or float, 2D or 3D array): Image of size [Ly x Lx (x nchan)].
+        masks (int, 2D array): Masks where 0=NO masks; 1,2,...=mask labels.
+        colors (int, 2D array, optional): Size [nmasks x 3], each entry is a color in 0-255 range.
+
+    Returns:
+        RGB (uint8, 3D array): Array of masks overlaid on grayscale image.
+    """
+    if colors is not None:
+        if colors.max() > 1:
+            colors = np.float32(colors)
+            colors /= 255
+        colors = utils.rgb_to_hsv(colors)
+    if img.ndim > 2:
+        img = img.astype(np.float32).mean(axis=-1)
+    else:
+        img = img.astype(np.float32)
+
+    HSV = np.zeros((img.shape[0], img.shape[1], 3), np.float32)
+    HSV[:, :, 2] = np.clip((img / 255. if img.max() > 1 else img) * 1.5, 0, 1)
+    hues = np.linspace(0, 1, masks.max() + 1)[np.random.permutation(masks.max())]
+    for n in range(int(masks.max())):
+        ipix = (masks == n + 1).nonzero()
+        if colors is None:
+            HSV[ipix[0], ipix[1], 0] = hues[n]
+        else:
+            HSV[ipix[0], ipix[1], 0] = colors[n, 0]
+        HSV[ipix[0], ipix[1], 1] = 1.0
+    RGB = (hsv_to_rgb(HSV) * 255).astype(np.uint8)
+    return RGB
