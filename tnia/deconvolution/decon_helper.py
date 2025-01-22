33812dbfbd5a23ea03677223a8e2028b6407cc7a
bnorthan <bnorthan@gmail.com>
2025-01-22 06:58:08 -0500

    Add decon_helper
    To start with we add a function that creates the saturation mask that is
    needed to mark invalid pixels in RL.

---
+++ tnia/deconvolution/decon_helper.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def create_saturation_mask(image):
+    """
+    Create a mask for saturated pixels in an image.
+
+    This is useful for masking out saturated pixels in an image before deconvolution.
+
+    Parameters
+    ----------
+    image : numpy.ndarray
+        Image to create a saturation mask for.
+    
+    Returns
+    -------
+    numpy.ndarray
+        Mask for saturated pixels.
+    """
+
+    mask  = np.ones_like(image)
+    dtype = image.dtype
+
+    if dtype == np.uint8:
+        mask[image==255] = 0
+
+    return mask
+
